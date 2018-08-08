c5270dc759a49408f5b49d881ac709a47d255922
k-okina <okinakahiro@gmail.com>
2018-08-08 15:44:45 +0900

    互換性

--- src/index.js
+++ src/index.js
@@ -15,9 +15,9 @@ export function snakecaseKeys(object, { deep = false, onlyPlainObject = true } =
   const reducer = (base, camelKey) => {
     const snakeKey = camelToSnake(camelKey);
     if (deep && isPlainObject(object[camelKey])) {
-      return { ...base, [snakeKey]: snakecaseKeys(object[camelKey], { deep, onlyPlainObject }) };
+      return Object.assign({}, base, { [snakeKey]: snakecaseKeys(object[camelKey], { deep, onlyPlainObject }) });
     }
-    return { ...base, [snakeKey]: object[camelKey] };
+    return Object.assign({}, base, { [snakeKey]: object[camelKey] });
   };
   if (onlyPlainObject && isPlainObject(object)) {
     return object;
